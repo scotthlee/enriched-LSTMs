a312a703557c5cff113e1449d527811f249f7cbe
Scott Lee <yle4@cdc.gov>
2019-09-27 08:47:58 -0400

    progress

--- train.py
+++ train.py
@@ -13,6 +13,7 @@ from models.supervised import RNN, EnrichedRNN
 
 def parse_arguments(parser):
     parser.add_argument('--data_dir', type=str, default=None)
+    parser.add_argument('--target_column', type=str, default='code')
     parser.add_argument('--batch_size', type=int, default=256)
     parser.add_argument('--epochs', type=int, default=10)
     args = parser.parse_args()
@@ -22,34 +23,29 @@ if __name__ == 'main':
     parser = argparse.ArgumentParser()
     args = parse_arguments(parser)
     
-    # Directories for reading and writing data
-    indir = args.data_dir
-    outdir = args.data_dir
-    
     # Importing the integer sequences
-    int_sents = h5py.File(indir + 'word_sents.hdf5', mode='r')
+    int_sents = h5py.File(args.data_dir + 'word_sents.hdf5', mode='r')
     X = np.array(int_sents['sents'])
     int_sents.close()
     
     # Importing the sparse records and targets
-    sparse_records = load_npz(indir + 'sparse_records.npz')
-    records = pd.read_csv(indir + 'records_clipped.csv')
+    sparse_records = load_npz(args.data_dir + 'sparse_records.npz')
+    records = pd.read_csv(args.data_dir + 'records_clipped.csv')
     
-    # Defining some codes to use as targets
-    target_codes = ['2', '88', '180', '233']
-    code_regexes = [r'\b2\b', r'\b88\b', r'\b180\b', r'\b233\b']
-    
-    # Making logical variables from the regular expressions
-    has = np.array([bool(re.search(hundo, doc))
-                             for doc in records.num_cc], dtype=np.uint8)
+    # Making a variable for the classification target
+    y = records[args.target_column]
+    unique_codes = np.unique(y)
+    if len(unique_codes) == 2:
+        num_classes = 1
+    else:
+        num_classes = len(unique_codes)
     
     # Importing the vocabulary
-    vocab_df = pd.read_csv(indir + 'word_dict.csv')
+    vocab_df = pd.read_csv(args.data_dir + 'word_dict.csv')
     vocab = dict(zip(vocab_df.word, vocab_df.value))
     
     # Defining the shared hyperparameters
     V = len(vocab)
-    num_classes = 1
     fit_batch = args.batch_size
     pred_batch = args.batch_size
     epochs = args.epochs
